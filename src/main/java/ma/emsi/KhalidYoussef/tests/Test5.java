7a04bfaa975342224fa7898287ff3cc92ca6eb98
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-11-02 21:20:09 +0100

    6eme commit: Ajout du 5eme test

--- src/main/java/ma/emsi/KhalidYoussef/tests/Test5.java
+++ src/main/java/ma/emsi/KhalidYoussef/tests/Test5.java
@@ -6,8 +6,6 @@ import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.googleai.GoogleAiEmbeddingModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
@@ -16,11 +14,10 @@ import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
 import java.time.Duration;
+import java.util.Scanner;
 
-public class Test4 {
-    // Assistant conversationnel
+public class Test5 {
     interface Assistant {
-        // Prend un message de l'utilisateur et retourne une réponse du LLM.
         String chat(String userMessage);
     }
     public static void main(String[] args) {
@@ -29,37 +26,28 @@ public class Test4 {
             System.err.println("Erreur : La variable d'environnement 'GEMINI_KEY' n'est pas définie.");
             return;
         }
-        ChatModel model = GoogleAiGeminiChatModel
-                .builder()
+        ChatModel model = GoogleAiGeminiChatModel.builder()
                 .apiKey(Key) // Pour Fournir la clé API
                 .modelName("gemini-2.5-flash") // Modèle à utiliser
                 .temperature(0.2) // la créativité
                 .build();
 
-        EmbeddingModel embeddingModel = GoogleAiEmbeddingModel.builder()
-                .apiKey(Key)
-                .modelName("text-embedding-004")
-                .taskType(GoogleAiEmbeddingModel.TaskType.RETRIEVAL_DOCUMENT)
-                .outputDimensionality(300)
-                .timeout(Duration.ofSeconds(10))
-                .build();
-
         ChatMemory memoire =
-                MessageWindowChatMemory.withMaxMessages(10);
-        String myDoc = "infos.txt";
+                MessageWindowChatMemory.withMaxMessages(20);
+
+        String myDoc = "ML.pdf";
         System.out.println("📄 Chargement du document : " + myDoc);
         Document document;
         try {
             document = FileSystemDocumentLoader.loadDocument(myDoc);
             System.out.println("Document chargé avec succès!");
-            System.out.println("Nombre de caractères : " + document.text().length());
         } catch (Exception e) {
             System.err.println("Erreur lors du chargement du document : " + e.getMessage());
             return;
         }
         EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-
         EmbeddingStoreIngestor.ingest(document, embeddingStore);
+
         Assistant assistant =
                 AiServices.builder(Assistant.class)
                         .chatModel(model)
@@ -67,10 +55,26 @@ public class Test4 {
                         .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
                         .build();
 
+        conversationAvec(assistant);
 
-        String question = "Pierre appelle son chat. Qu'est-ce qu'il pourrait dire ?";
-
-        String reponse = assistant.chat(question);
-        System.out.println(reponse);
+    }
+    private static void conversationAvec(Assistant assistant){
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
     }
 }
